9f7746862dc17812224b28b91fca3007576e927e
Jani7466 <alirazajani.7466@gmail.com>
2024-10-29 21:38:32 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=uzRUhKRI#TYYTsco-MyXXPpb39kxbdRY2XSwcY__Hl9xiyDTC_ls",
+MONGODB: process.env.MONGODB || "
+global.mongodb= process.env.MONGODB_URI || "mongodb+srv://mohsin:mohsin@cluster0.iauaztt.mongodb.net/?retryWrites=true&w=majority",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
